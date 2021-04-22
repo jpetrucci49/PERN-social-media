c59ec464b12f4900bc88f6e632241f8ed0dfc836
Jpetrucci <jpetrucci@cainc.com>
2021-04-22 17:57:14 -0400

    Add route to get a user

--- server/routes/users.js
+++ server/routes/users.js
@@ -20,4 +20,22 @@ router.post('/api/users/add', async (req, res, next) => {
   }
 });
 
+/* GET a user. */
+router.get('/api/users/:uid', async (req, res, next) => {
+  try {
+    const { uid } = req.params;
+    await pool.query(
+      `SELECT * FROM users
+              WHERE uid=$1`,
+      [uid],
+      (q_err, q_res) => {
+        if (q_err) return next(q_err);
+        res.json(q_res.rows);
+      },
+    );
+  } catch (err) {
+    console.error(err);
+  }
+});
+
 module.exports = router;
